1f54f054ace33e145da3b59a23bfb89a7d99a854
chen.yuhui <chen.yuhui@bytedance.com>
2026-06-26 11:11:01 +0800

    update UI

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,21 +1,58 @@
 $(document).ready(function () {
+  const publicationPanelButtonSelector = "a.award, a.bibtex, a.abstract-toggle, a.video-toggle";
+  const publicationPanelSelector = ".award.hidden, .bibtex.hidden, .abstract.hidden, .video.hidden";
+  const publicationActiveButtonStyles = {
+    backgroundColor: "color-mix(in srgb, var(--global-theme-color) 12%, transparent)",
+    borderColor: "color-mix(in srgb, var(--global-theme-color) 20%, transparent)",
+    color: "var(--global-theme-color)",
+    fontWeight: "600",
+  };
+
+  const resetPublicationButtonStyles = (buttons) => {
+    buttons.removeClass("is-active").attr("aria-expanded", "false").each(function () {
+      this.style.backgroundColor = "";
+      this.style.borderColor = "";
+      this.style.color = "";
+      this.style.fontWeight = "";
+    });
+  };
+
+  const activatePublicationButtonStyles = (button) => {
+    button.addClass("is-active").attr("aria-expanded", "true").css(publicationActiveButtonStyles);
+  };
+
   const togglePublicationPanel = (element, panelClass) => {
-    const card = $(element).closest(".publication-card");
-    card.find(`.${panelClass}.hidden`).toggleClass("open");
-    card.find(".award.hidden.open, .bibtex.hidden.open, .abstract.hidden.open, .video.hidden.open").not(`.${panelClass}.hidden.open`).toggleClass("open");
+    const button = $(element);
+    const card = button.closest(".publication-card");
+    const panel = card.find(`.${panelClass}.hidden`);
+    const isOpening = !panel.hasClass("open");
+
+    card.find(publicationPanelSelector).removeClass("open");
+    resetPublicationButtonStyles(card.find(publicationPanelButtonSelector));
+
+    if (isOpening) {
+      panel.addClass("open");
+      activatePublicationButtonStyles(button);
+    }
   };
 
   // add toggle functionality to abstract, award and bibtex buttons
-  $("a.abstract-toggle").click(function () {
+  $(publicationPanelButtonSelector).attr("aria-expanded", "false");
+
+  $("a.abstract-toggle").click(function (event) {
+    event.preventDefault();
     togglePublicationPanel(this, "abstract");
   });
-  $("a.video-toggle").click(function () {
+  $("a.video-toggle").click(function (event) {
+    event.preventDefault();
     togglePublicationPanel(this, "video");
   });
-  $("a.award").click(function () {
+  $("a.award").click(function (event) {
+    event.preventDefault();
     togglePublicationPanel(this, "award");
   });
-  $("a.bibtex").click(function () {
+  $("a.bibtex").click(function (event) {
+    event.preventDefault();
     togglePublicationPanel(this, "bibtex");
   });
   $("a").removeClass("waves-effect waves-light");
